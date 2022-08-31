d9187e6d17d4f4610e9e0ba1b9867a2f276daef9
satyam raj <satyam3279raj@gmail.com>
2022-08-31 20:44:38 +0530

    added questions

--- day8_2_Selection_Sort.cpp
+++ day8_2_Selection_Sort.cpp
@@ -1,3 +1,33 @@
+// Bob considered an array of Strings and he sorted the Strings in alphabetical order
+
+// Input Format
+
+// 5
+// paper true soap floppy flower
+
+// Constraints
+
+// Array size should be 5
+// Implement selection sort for sorting the strings in alphabetical order and repeat for n-1 passes. For each pass the output should be displayed
+// Output Format
+
+// floppy true soap paper flower
+// floppy flower soap paper true
+// floppy flower paper soap true
+// floppy flower paper soap true
+
+// Sample Input 0
+
+// 5
+// paper true soap floppy flower
+// Sample Output 0
+
+// floppy true soap paper flower
+// floppy flower soap paper true
+// floppy flower paper soap true
+// floppy flower paper soap true
+
+
 #include <iostream>
 using namespace std;
 
@@ -7,7 +37,7 @@ void printArray(string arr[], int size)
     int i;
     for (i=0; i < size; i++)
         cout<<arr[i]<<" ";
-    printf("\n");
+    cout<<endl;
 }
  
 
