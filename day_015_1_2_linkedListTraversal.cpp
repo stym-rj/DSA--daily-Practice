5b30a0086f3f64ed53a7b22e19a642ce9bf29d74
satyam raj <satyam3279raj@gmail.com>
2022-09-09 23:12:01 +0530

    added my code for day_15_1 ->(5/6 test cases passed) and renamed 1 file

---
+++ day_015_1_2_linkedListTraversal.cpp
@@ -0,0 +1,110 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *link;
+};
+
+node *first, *temp;
+
+class LinkedList
+{
+public:
+    node *first, *last;
+    LinkedList();
+    void create();
+    int count();
+};
+LinkedList::LinkedList()
+{
+    first = NULL;
+    last = NULL;
+};
+
+void LinkedList::create()
+{
+    node *temp = new node();
+    cin >> temp->data;
+    temp->link = NULL;
+    if (first == NULL)
+    {
+        first = temp;
+        last = first;
+    }
+    else
+    {
+        last->link = temp;
+        last = temp;
+    }
+};
+
+int isprime(int n)
+{
+
+    if (n == 1 || n == 0)
+    {
+        return 0;
+    }
+    if (n == 2)
+    {
+        return 1;
+    }
+
+    for (int j = 2; j < n - 1; j++)
+    {
+        if (n % j == 0)
+            return 0;
+    }
+    return 1;
+}
+
+int LinkedList::count()
+{
+    node *temp;
+    temp = first;
+
+    int count = 0;
+    int a;
+
+    while (temp != NULL)
+    {
+        a = temp->data;
+        temp = temp->link;
+
+        if (isprime(a))
+            count++;
+    }
+    return count;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    if (n < 0)
+    {
+        cout << "Invalid Number";
+        return 0;
+    }
+
+    int i;
+
+    LinkedList l1;
+
+    for (i = 0; i < n; i++)
+    {
+        l1.create();
+    }
+
+    cout << l1.count();
+
+    return 0;
+}
