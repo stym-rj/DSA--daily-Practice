89f74dd1e576db80c1a3a328ffaec4548cf74b23
satyam raj <satyam3279raj@gmail.com>
2022-09-16 23:10:17 +0530

    added question in day20, q1

--- day_020_1_linkedListDeletion.cpp
+++ day_020_1_linkedListDeletion.cpp
@@ -1,3 +1,41 @@
+// WAP to accept N Numbers into to an Singly Link list and delete the largest Number from the Link list.
+
+// For Example if 5 numbers are 3 5 8 6 4 then as 8 is largest Number, delete 8 then the output will be 3 5 6 4
+
+// Input Format
+
+// First line will be the number N. Second Line will accect N numbers from the user.
+
+// Sample Input 1: 5
+// 4 8 3 6 2
+
+// Sample Output 1: 4 3 6 2
+
+// Sample Input 2: 6
+// 7 5 3 9 1 4
+
+// Sample Output 2:
+
+// 7 5 3 1 4
+
+// Constraints
+
+// All the inputs are positive integers and numbers entered in the second line of input are non-duplicate
+
+// Output Format
+
+// Prints all the numbers after removing the number from the Linked List
+
+// Sample Input 0
+
+// 5
+// 2 8 7 9 3
+// Sample Output 0
+
+// 2 8 7 3
+
+
+
 #include <iostream>
 
 class Node{
